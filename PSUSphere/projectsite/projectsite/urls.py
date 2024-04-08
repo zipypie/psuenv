11cba8bcf0df3073879f71f8e890d8c46504b956
zipypie <201980170@psu.palawan.edu.ph>
2024-04-08 21:03:46 +0800

    add org_list.html

--- PSUSphere/projectsite/projectsite/urls.py
+++ PSUSphere/projectsite/projectsite/urls.py
@@ -19,10 +19,11 @@ Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView
+from studentorg.views import HomePageView, OrganizationList
 from studentorg import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , views.HomePageView.as_view(), name= 'home'),
+    path('organization_list' , OrganizationList.as_view(), name= 'organization-list'),
 ]
