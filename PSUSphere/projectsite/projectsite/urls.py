2e9ce48ecafd8ad13d686e51054246f6d58c5300
zipypie <201980170@psu.palawan.edu.ph>
2024-04-08 20:49:46 +0800

    modify base

--- PSUSphere/projectsite/projectsite/urls.py
+++ PSUSphere/projectsite/projectsite/urls.py
@@ -1,22 +1,28 @@
 """
 URL configuration for projectsite project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+The urlpatterns list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+    
+Add an import:  from my_app import views
+Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    
+Add an import:  from other_app.views import Home
+Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    
+Import the include() function: from django.urls import include, path
+Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
+from studentorg.views import HomePageView
+from studentorg import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('' , views.HomePageView.as_view(), name= 'home'),
 ]
