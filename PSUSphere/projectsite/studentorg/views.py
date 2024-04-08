02f98f805a25e4438eb258d8c2f728c93d974b33
zipypie <201980170@psu.palawan.edu.ph>
2024-04-08 21:33:12 +0800

    modify pagination.html

--- PSUSphere/projectsite/studentorg/views.py
+++ PSUSphere/projectsite/studentorg/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from studentorg.models import Organization
 
+
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
@@ -9,6 +10,6 @@ class HomePageView(ListView):
     
 class OrganizationList(ListView):
     model = Organization
-    context_object_name =' organization '
+    context_object_name ='organization'
     template_name = 'org_list.html '
     paginate_by = 5
